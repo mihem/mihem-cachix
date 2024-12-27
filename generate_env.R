8d48c5356543525c3b85f43b103555cab60d0153
mischko <mhem@mailbox.org>
2024-12-27 22:20:35 +0100

    initial commit

---
+++ generate_env.R
@@ -0,0 +1,11 @@
+library(rix)
+rix(
+    r_ver = "4.4.1",
+    r_pkgs = c("Seurat"),
+    system_pkgs = NULL,
+    git_pkgs = NULL,
+    ide = "code",
+    project_path = ".",
+    overwrite = TRUE,
+    print = TRUE
+)
