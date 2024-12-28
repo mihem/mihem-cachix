0c24538f02aa6d878413524e25736ebc51de0dbc
mischko <mhem@mailbox.org>
2024-12-28 19:51:12 +0100

    add scMisc

--- generate_env.R
+++ generate_env.R
@@ -1,9 +1,13 @@
 library(rix)
 rix(
-    r_ver = "4.4.1",
+    r_ver = "4.4.0",
     r_pkgs = c("Seurat"),
     system_pkgs = NULL,
-    git_pkgs = NULL,
+    git_pkgs = list(
+		    package_name = "scMisc",
+		    repo_url = "https://github.com/mihem/scMisc",
+		    commit = "e2ebddcb779b935551f14216514c0429616fc91d"
+		    ),
     ide = "code",
     project_path = ".",
     overwrite = TRUE,
